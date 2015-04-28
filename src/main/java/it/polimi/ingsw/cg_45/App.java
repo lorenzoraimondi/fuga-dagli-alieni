d93f29ec01257a4d9d98a09592a12f7aeb37fc50
Lorenzo <Lorenzo@10.169.162.190>
2015-04-28 16:37:07 +0200

    Ancora lui!

--- src/main/java/it/polimi/ingsw/cg_45/App.java
+++ src/main/java/it/polimi/ingsw/cg_45/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+    	System.out.println( "DESTRO SECCOOOO" );
         System.out.println( "CCCCCCCCCEZZZZZZIONALE" );
+        
     }
 }
